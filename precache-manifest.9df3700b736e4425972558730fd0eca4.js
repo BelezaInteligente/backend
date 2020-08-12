b60918e03426572eba62dfe2f1331977b1420b52
Eduardo Kraus Nunes <ekrausnunes@gmail.com>
2020-08-12 01:23:36 -0300

    Publish Frontend

---
+++ precache-manifest.9df3700b736e4425972558730fd0eca4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd2e8f7afb81aa956c4195f33fe7379",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d801a9fd85e03930d31",
+    "url": "/static/js/2.2c595a8b.chunk.js"
+  },
+  {
+    "revision": "6c83e364bc7026b3266fe99060aa5fab",
+    "url": "/static/js/2.2c595a8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b32684fb556855dc8e64",
+    "url": "/static/js/main.df6b6278.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
